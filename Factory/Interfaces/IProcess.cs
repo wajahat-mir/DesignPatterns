03801583d9b86bf384690b29c8d71231431ebafd
Wajahat Mir <wajahat.mir1@gmail.com>
2021-04-24 18:53:48 -0400

    * Factory Pattern

---
+++ Factory/Interfaces/IProcess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Factory.Interfaces
+{
+    interface IProcess
+    {
+        public void Init();
+        public void Start();
+        public void Shutdown();
+
+    }
+}
---
+++ Factory/Program.cs
@@ -0,0 +1,16 @@
+﻿using Factory.Factories;
+using System;
+
+namespace Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var factory = new ProcessFactory();
+
+            var process = factory.Create("INSERT");
+        }
+    }
+}
