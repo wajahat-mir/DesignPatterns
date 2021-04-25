9121b3833b0400dec882b882d37cef4c496ed259
Wajahat Mir <wajahat.mir1@gmail.com>
2021-04-25 08:10:11 -0400

    * Visitor Pattern

---
+++ Visitor/Models/GoogleModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Visitor.Interfaces;
+
+namespace Visitor.Models
+{
+    class GoogleModel:IReview
+    {
+        public void accept(IReviewVisitor reviewVisitor)
+        {
+            reviewVisitor.visitGoogle();
+        }
+    }
+}
