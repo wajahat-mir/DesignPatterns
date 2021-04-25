c401c4cb507d1175a509215a5dc905e3508b4712
Wajahat Mir <wajahat.mir1@gmail.com>
2021-04-25 07:32:36 -0400

    * Flyweight pattern

---
+++ Flyweight/Models/ReviewModel.cs
@@ -0,0 +1,18 @@
+﻿using Flyweight.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flyweight
+{
+    class ReviewModel
+    {
+        public LocationModel location { get; set; }
+
+        public ReviewModel(LocationModel location)
+        {
+            this.location = location;
+        }
+
+    }
+}
