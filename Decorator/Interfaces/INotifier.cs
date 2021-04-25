c682c4ea89bb3b5add9a99f07942e41fb75b6827
Wajahat Mir <wajahat.mir1@gmail.com>
2021-04-25 07:02:32 -0400

    * commit

---
+++ Decorator/Interfaces/INotifier.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator.Interfaces
+{
+    interface INotifier
+    {
+        public void SendNotification();
+        public void TrackNotification();
+    }
+}
