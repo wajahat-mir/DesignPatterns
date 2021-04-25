27cf13a337860aa289128924f87fe8dfab5566b7
Wajahat Mir <wajahat.mir1@gmail.com>
2021-04-25 06:19:25 -0400

    * Bridge Pattern

---
+++ Bridge/Process/InsertionProcess.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bridge.Process
+{
+    class InsertionProcess
+    {
+        private readonly IBatchService _batchService;
+
+        public InsertionProcess(IBatchService batchService)
+        {
+            _batchService = batchService;
+        }
+
+        public void Init()
+        {
+            _batchService.CreatePool();
+            _batchService.SetTimeout();
+        }
+
+        public void Build(int numOfTasks)
+        {
+            for (int i = 0; i < numOfTasks; i++)
+                _batchService.AddTask();
+        }
+
+        public void Start()
+        {
+            _batchService.StartJob();
+        }
+    }
+}
