c34e7f473e3f187cf0bc6cec08a799a8d9099243
Wajahat Mir <wajahat.mir1@gmail.com>
2021-04-25 08:16:57 -0400

    * Strategy pattern

---
+++ Strategy/Strategies/Context.cs
@@ -0,0 +1,22 @@
+﻿using Strategy.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy.Strategies
+{
+    class Context
+    {
+        private IStrategy _strategy;
+
+        public void setStrategy(IStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public void executeStrategy()
+        {
+            _strategy.execute();
+        }
+    }
+}
---
+++ Strategy/Strategies/Substraction.cs
@@ -0,0 +1,15 @@
+﻿using Strategy.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy.Strategies
+{
+    class Substraction: IStrategy
+    {
+        public void execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
